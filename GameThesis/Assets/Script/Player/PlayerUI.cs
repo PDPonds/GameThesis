764b50339effbd1e9c99acfaea26f6aee69a1ec4
Po2s <pondandmeen@gmail.com>
2023-10-07 16:03:30 +0700

    BloodShader

--- GameThesis/Assets/Script/Player/PlayerUI.cs
+++ GameThesis/Assets/Script/Player/PlayerUI.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class PlayerUI : Auto_Singleton<PlayerUI>
 {
+    public Material m_bloodMat;
+    public Transform t_bloodObj;
+
     private void Update()
     {
-
+        if (PlayerManager.Instance.i_currentHP < PlayerManager.Instance.i_maxHP)
+        {
+            t_bloodObj.gameObject.SetActive(true);
+            m_bloodMat.SetFloat("_Range", 1.5f + PlayerManager.Instance.i_currentHP);
+        }
+        else
+        {
+            t_bloodObj.gameObject.SetActive(false);
+        }
     }
 }
